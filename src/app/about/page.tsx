a1d5f2f035a2b5c8184bceb1dcb19f8ed280b040
Muzafar Ali <[valid-email]>
2024-11-07 21:46:23 +0500

    responsive customize

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -3,8 +3,8 @@
 
 function AboutPage(){
     return(
-        <div className="w-full h-full flex justify-center items-center">
-           <h1 className="font-bold">About Page is Pendding Just Wait 2 days</h1>
+        <div className="w-full  flex justify-center items-center h-screen">
+           <h1 className="font-bold md:text-2xl">About Page is Pendding Just Wait 2 days</h1>
         </div>
     )
 }
---
+++ src/app/contact/page.tsx
@@ -0,0 +1,8 @@
+function Contactpage(){
+    return(
+        <div className="flex justify-center items-center h-screen">
+            <h1 className="font-bold md:text-2xl">Contact page Pendding</h1>
+        </div>
+    )
+}
+export default Contactpage
